0fca9a7914eeed03f806e77d02887584804b935a
David Alonso <dam.phys@gmail.com>
2020-04-22 10:53:58 +0100

    example

--- setup.py
+++ setup.py
@@ -6,11 +6,15 @@ description = "schNell - Fast calculation of N_l for GW anisotropies"
 with open('requirements.txt') as f:
     requirements = f.read().splitlines()
 
+with open('README.md') as f:
+    long_description = f.read()
+
 setup(name="schnell",
       version="0.1.0",
-      long_description=description,
+      description=description,
+      long_description=long_description,
       long_description_content_type='text/markdown',
-      url="https://github.com/damonge/schnell",
+      url="https://github.com/damonge/schNell",
       author="David Alonso",
       author_email="david.alonso@physics.ox.ac.uk",
       install_requires=requirements,
