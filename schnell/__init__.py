587587e84f2047147b538d416486f88529fbd92f
David Alonso <dam.phys@gmail.com>
2020-05-03 10:20:06 +0100

    v0.2

--- schnell/__init__.py
+++ schnell/__init__.py
@@ -9,4 +9,4 @@ from .correlation import (  # noqa
 from .detector import (  # noqa
     Detector, GroundDetectorTriangle,
     GroundDetector, LISADetector)
-__version__ = '0.1.1'
+__version__ = '0.2.0'
--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open('README.md') as f:
     long_description = f.read()
 
 setup(name="schnell",
-      version="0.1.0",
+      version="0.2.0",
       description=description,
       long_description=long_description,
       long_description_content_type='text/markdown',
